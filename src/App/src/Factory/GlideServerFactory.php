ebc6165b6f49688f25d8a67465fe7f017ff8ff50
iampersistent <shank@zestic.com>
2021-10-19 09:25:15 -0500

    add optimizer

--- src/App/src/Factory/GlideServerFactory.php
+++ src/App/src/Factory/GlideServerFactory.php
@@ -4,19 +4,27 @@ declare(strict_types=1);
 namespace App\Factory;
 
 use ConfigValue\GatherConfigValues;
+use Infinityweb\Glide\Optimizer\OptimizerManipulator;
 use League\Glide\Server;
 use League\Glide\ServerFactory;
 use Psr\Container\ContainerInterface;
+use Zestic\Flysystem\Factory\FilesystemFactory;
 
 final class GlideServerFactory
 {
     public function __invoke(ContainerInterface $container): Server
     {
         $config = (new GatherConfigValues)($container, 'glide');
-        $config['cache'] = (new \Zestic\Flysystem\Factory\FilesystemFactory('cache'))($container);
-        $config['source'] = (new \Zestic\Flysystem\Factory\FilesystemFactory('source'))($container);
+        $config['cache'] = (new FilesystemFactory('cache'))($container);
+        $config['source'] = (new FilesystemFactory('source'))($container);
         $config['response'] =  new ResponseFactory();
 
-        return (new ServerFactory($config))->getServer();
+        $server = (new ServerFactory($config))->getServer();
+
+        $manipulators = $server->getApi()->getManipulators();
+        $manipulators[] = new OptimizerManipulator();
+        $server->getApi()->setManipulators($manipulators);
+
+        return $server;
     }
 }
